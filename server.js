a23fa18f455590494831f51b51231861f4c811d2
Blazed <samueltheofie@gmail.com>
2022-04-11 19:18:18 +0700

    Segregated UI and Backend

--- server.js
+++ server.js
@@ -2,87 +2,72 @@ if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
+// Libraries
 const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt')
+const expressLayouts = require('express-ejs-layouts')
 const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
-const methodOverride = require('method-override')
+// const methodOverride = require('method-override')
 
-const initializePassport = require('./passport-config')
+const app = express()
+
+// Passport Config
+const initializePassport = require('./modules/passport-config')
 initializePassport(
     passport,
-    email => users.find(user => user.email == email),
-    id => users.find(user => user.id == id)
+    email => users.find(user => user.email == email), //To change with database
+    id => users.find(user => user.id == id) //To change with database
 )
 
-const users = []
+// Routes
+const indexRouter = require('./routes/index')
+const loginRouter = require('./routes/login')
+const registerRouter = require('./routes/register')
 
-app.set('view-engine', 'ejs')
+// Database config (setup here)
+const users = [] 
+
+// Setting variables
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(express.static('public'))
+
+// EJS
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
+
+// Bodyparser
 app.use(express.urlencoded({ extended: false }))
-app.use(flash())
+
+// Express Session
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
 }))
+
+// Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
-app.use(methodOverride('_method'))
 
-app.get('/', checkAuthenticated, (req, res) => {
-    res.render('index.ejs', { name: req.user.name })
-})
+// Connect Flash
+app.use(flash())
 
-app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs')
-})
+// Routes
+app.use('/', indexRouter)
+app.use('/login', loginRouter)
+app.use('/register', registerRouter)
 
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
+// app.use(methodOverride('_method'))
 
-app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render('register.ejs')
-})
+// app.delete('/logout', (req, res) => {
+//     req.logOut()
+//     res.redirect('/login')
+// })
 
-app.post('/register', checkNotAuthenticated, async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        })
-        res.redirect('/login')
-    } catch {
-        res.redirect('/register')
-    }
-})
 
-app.delete('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('/login')
-})
 
-function checkAuthenticated(req, res, next) {
-    if(req.isAuthenticated()) {
-        return next()
-    }
 
-    res.redirect('/login')
-}
 
-function checkNotAuthenticated(req, res, next) {
-    if(req.isAuthenticated()) {
-        return res.redirect('/')
-    }
-
-    next()
-}
-
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
